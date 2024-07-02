461c4a049ccc9c5ae9aca4d3066941901c305c5c
Mari0 <cuongph@tigren.com>
2024-07-02 17:01:43 +0700

    CUS-2248/CUS-2495 Remove sandbox mode on configuration
    Remove config Is Sandbox and update api endpoint

--- Model/RedirectUrl.php
+++ Model/RedirectUrl.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * @author    Tigren Solutions <info@tigren.com>
- * @copyright Copyright (c) 2024 Tigren Solutions <https://www.tigren.com>. All rights reserved.
- * @license   Open Software License ("OSL") v. 3.0
- */
+
 declare(strict_types=1);
 
 namespace Atoa\AtoaPayment\Model;
